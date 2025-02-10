27289e7a64e83e87bb6fecf787eabbc3e7c8684c
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2025-02-10 14:12:32 +0100

    remove last commit

--- Modules/BinanceHistoricalDataFetcher.py
+++ Modules/BinanceHistoricalDataFetcher.py
@@ -9,20 +9,15 @@ SYMBOLS = ["BTCUSDT","ETHUSDT","XRPUSDT","SOLUSDT"]
 INTERVALS = ["1m", "5m", "15m", "1h"]
 LIMIT = 1000
 
-def get_date_ago_ms(days_number, product):
-    date_seven_years_ago = datetime.now(timezone.utc) - timedelta(days=days_number*product)
+def get_date_ago_ms():
+    date_seven_years_ago = datetime.now(timezone.utc) - timedelta(days=365*5)
     date_miliseconds = int(date_seven_years_ago.timestamp() * 1000)
     return date_miliseconds
 
-HISTORICAL_DATA_END_DATE = get_date_ago_ms(365,5)
-DATE_TECHNICAL_INDICATOR_10 = get_date_ago_ms(10,1)
-DATE_TECHNICAL_INDICATOR_20 = get_date_ago_ms(20,1)
-DATE_TECHNICAL_INDICATOR_50 = get_date_ago_ms(50,1)
-DATE_TECHNICAL_INDICATOR_200 = get_date_ago_ms(200,1)
+HISTORICAL_DATA_END_DATE = get_date_ago_ms()
 
 async def fetch_historical_candles(symbol, interval):
     end_time = int(time.time() * 1000)  # Aktualny czas w milisekundach
-    data_technical_indicators = []
     
     async with aiohttp.ClientSession() as session:
         while True:
@@ -53,30 +48,9 @@ async def fetch_historical_candles(symbol, interval):
                     }
                     for candle in data
                 ]
-
-                match interval:
-                    case "1m":
-                        #sprawdzenie czy nie została przekroczona data
-                        if candles[0]["close_time"] > DATE_TECHNICAL_INDICATOR_10:
-                            #dodaj do listy
-                            data_technical_indicators.extend(candles)
-                        return True
-                    case "5m":
-                        if candles[0]["close_time"] > DATE_TECHNICAL_INDICATOR_20:
-                            data_technical_indicators.extend(candles)
-                    case "15m":
-                        if candles[0]["close_time"] > DATE_TECHNICAL_INDICATOR_50:
-                            data_technical_indicators.extend(candles)
-                    case "1h":
-                        if candles[0]["close_time"] > DATE_TECHNICAL_INDICATOR_200:
-                            data_technical_indicators.extend(candles)
-                            
-                if end_time < DATE_TECHNICAL_INDICATOR_200:
-                    #send to technical anaylsis calculate method
-                    calculate_sma_ema(data_technical_indicators)
                 
                 # Zapisanie świec do bazy danych w partiach po 1000
-                # await save_candles_to_db(candles)
+                await save_candles_to_db(candles)
 
                 # Sprawdzenie, czy pobraliśmy mniej niż limit (koniec danych)
                 if len(data) < LIMIT:
@@ -85,7 +59,7 @@ async def fetch_historical_candles(symbol, interval):
 
                 # Ustaw nowy endTime jako najstarszy czas - 1ms (żeby uniknąć duplikatów)
                 end_time = data[0][0] - 1
-
+                
                 if end_time < HISTORICAL_DATA_END_DATE:
                     print(f"Osiągnięto maksymalną datę historii dla {symbol} {interval}")
                     break
